9a2d6d34fb8a479082f69218eb898177e4c878a8
bayoticstudios <abdullahishobaloju@gmail.com>
2023-02-14 15:34:09 +0100

    initial-project-commits

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
