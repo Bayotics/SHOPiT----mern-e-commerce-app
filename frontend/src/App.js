dc142433c1678e3d5b1fb224a3a52ddf01d3431c
bayoticstudios <abdullahishobaloju@gmail.com>
2023-02-15 11:09:18 +0100

    initial-project-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,33 @@
-
+import data from "./data";
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className = "products">
+        {data.products.map((product) => {
+          return(
+            <div className = "product" key = {product.slug}>
+              <a href = {`/product/${product.slug}`}>
+                <img src = {product.image} alt = {product.name}/>
+              </a>
+              <div className = "product-info">
+                <a href = {`/product/${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+            </div>
+            </div>  
+          )
+        })}
+        </div>
+      </main>
     </div>
   );
 }
