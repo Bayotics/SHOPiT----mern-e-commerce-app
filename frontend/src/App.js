136d446b26b42289bdafce0fe2f5b6f6ece0c113
bayoticstudios <abdullahishobaloju@gmail.com>
2023-02-16 10:34:01 +0100

    Day-2-mern-e-commerce-development-changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,36 @@
-import data from "./data";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazona</a>
-      </header>
-      <main>
-        <h1>Featured products</h1>
-        <div className = "products">
-        {data.products.map((product) => {
-          return(
-            <div className = "product" key = {product.slug}>
-              <a href = {`/product/${product.slug}`}>
-                <img src = {product.image} alt = {product.name}/>
-              </a>
-              <div className = "product-info">
-                <a href = {`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to Cart</button>
-            </div>
-            </div>  
-          )
-        })}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
