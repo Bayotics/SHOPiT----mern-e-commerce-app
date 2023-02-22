3a7e41097abe4d45445c10a8bcc13d14df5d9bd5
bayoticstudios <abdullahishobaloju@gmail.com>
2023-02-22 11:57:55 +0100

    latest-commits

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { StoreProvider } from './Store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+    <StoreProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>
+)
