401d514ef64f9daa82c4b08018608833fa380df0
bayoticstudios <abdullahishobaloju@gmail.com>
2023-02-22 11:59:08 +0100

    latest-changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
-// import logger from 'use-reducer-logger';
+import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -23,7 +23,7 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer((reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: '',
