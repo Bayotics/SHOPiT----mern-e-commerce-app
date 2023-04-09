e041e1947ef23b30620a7f1cc5fba139337a41ae
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-10 00:08:51 +0100

    paypal-react-installed-missing-api-key

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
 
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-<React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <App />
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
       </HelmetProvider>
     </StoreProvider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
