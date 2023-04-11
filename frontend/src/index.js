910ddc9ccd9ae118b761a4edb9c237821854ef87
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-11 12:16:49 +0100

    removed-paypal-to-be-replaced-with-paystack

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <PayPalScriptProvider deferLoading={true}>
-          <App />
-        </PayPalScriptProvider>
+        <App />
       </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>,
@@ -24,4 +20,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
