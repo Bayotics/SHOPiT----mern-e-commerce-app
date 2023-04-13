7fee8c40806e9effb8532eb57b5cd676987f039b
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-13 16:21:09 +0100

    venture-renamining

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>SHOPiT</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>SHOPiT</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
