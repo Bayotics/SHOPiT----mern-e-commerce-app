440f83656c088733348eaecf084beb656b2af3d5
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-19 23:26:31 +0100

    ok

--- backend/models/paymentModel.js
+++ backend/models/paymentModel.js
@@ -6,7 +6,7 @@ const paymentSchema = new mongoose.Schema(
       {
         slug: { type: String, required: true },
         name: { type: String, required: true },
-        quantity: { type: Number, required: true },
+        quantity: { type: Number, required: true }, 
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
