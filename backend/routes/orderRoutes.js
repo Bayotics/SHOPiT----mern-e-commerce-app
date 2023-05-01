cb7b1f36a2105d18202e125e490cd3ee1910cd95
bayoticstudios <abdullahishobaloju@gmail.com>
2023-05-01 21:30:41 +0100

    delete request bugfix

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -149,7 +149,7 @@ orderRouter.delete(
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
     if (order) {
-      await order.remove();
+      await order.deleteOne();
        res.send({ message: 'Order Deleted' });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
