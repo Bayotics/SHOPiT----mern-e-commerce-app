d787cef394457f51919a0030c4fb62f91ecbcdfc
bayoticstudios <abdullahishobaloju@gmail.com>
2023-05-03 20:51:27 +0100

    adding edit screeen to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,6 +33,7 @@ import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 
 function App() {
@@ -239,6 +240,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+               <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreen />
                   </AdminRoute>
                 }
               ></Route>
