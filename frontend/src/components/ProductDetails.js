c0c16a4bbfc03656a6cd822379ea3f59e4b11411
bayoticstudios <abdullahishobaloju@gmail.com>
2023-06-08 22:31:17 +0100

    product screen fix

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -7,14 +7,7 @@ const ProductDetails = ({product, rating}) => {
      const descriptionContent = <div className="tab-pane fade show active"    aria-labelledby="product-desc-link">
                                     <div className="product-desc-content">
                                         <h3>Product Information</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. </p>
-                                        <ul>
-                                            <li>Nunc nec porttitor turpis. In eu risus enim. In vitae mollis elit. </li>
-                                            <li>Vivamus finibus vel mauris ut vehicula.</li>
-                                            <li>Nullam a magna porttitor, dictum risus nec, faucibus sapien.</li>
-                                        </ul>
-
-                                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. </p>
+                                        <p>{product.description}</p>
                                     </div>
                                 </div>;
     const additionalInfoContent = <div className="tab-pane fade show active" id="product-info-tab"   aria-labelledby="product-info-link">
