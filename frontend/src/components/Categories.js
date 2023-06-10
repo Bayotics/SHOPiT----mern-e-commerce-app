90650b39e26e05df7220f663cea90e3c06529466
bayoticstudios <abdullahishobaloju@gmail.com>
2023-06-10 22:39:23 +0100

    categories comp

---
+++ frontend/src/components/Categories.js
@@ -0,0 +1,18 @@
+
+const Categories = () => {
+    return(
+        <div className='mt-4'>
+            <h1 className='text-center fw-bold'>TOP CATEGORIES</h1>
+            <div className="border categories-list">
+                <div className="categories-nav d-inline">
+                    <p className="categories-nav d-inline">Electronics & Tech</p>
+                    <p className="categories-nav d-inline">Fashion</p>
+                    <p className="categories-nav d-inline">Food</p>
+                    <p className="categories-nav d-inline">Furniture</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
