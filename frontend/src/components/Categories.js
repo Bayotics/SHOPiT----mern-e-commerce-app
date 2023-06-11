876549840b65f012a53bb4ccf41ab9f6f047201d
bayoticstudios <abdullahishobaloju@gmail.com>
2023-06-11 22:31:11 +0100

    category component in progress

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,14 +1,184 @@
+import Nav from 'react-bootstrap/Nav';
 
 const Categories = () => {
+
+    const allCat = 
+        <div class="container">
+            <div class="row g-2">
+                <div class="col-6">
+                <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+                <div class="col-6">
+                <div class="p-3 border bg-light">Custom column padding</div>
+                </div>
+            </div>
+        </div>
     return(
-        <div className='mt-4'>
+        <div className='mt-4 border'>
             <h1 className='text-center fw-bold'>TOP CATEGORIES</h1>
-            <div className="border categories-list">
-                <div className="categories-nav d-inline">
-                    <p className="categories-nav d-inline">Electronics & Tech</p>
-                    <p className="categories-nav d-inline">Fashion</p>
-                    <p className="categories-nav d-inline">Food</p>
-                    <p className="categories-nav d-inline">Furniture</p>
+            <div className="border categories-list container">
+                <div className="categories-nav d-inline row">
+                     <Nav className="me-auto  w-100  justify-content-center">
+                        <div className='nav-link text-dark'>ALL</div>
+                        <div className='nav-link text-dark'>Electronics & Tech</div>
+                        <div className='nav-link text-dark'>Fashion</div>
+                        <div className='nav-link text-dark'>Food</div>
+                        <div className='nav-link text-dark'>Furniture</div>
+                      </Nav>
+                </div>
+            </div>
+            <div class="container mt-4">
+                <div class="row g-3">
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div  id = "category-item">
+                            <div class="p-3 border bg-light" id = "category-inner"></div>
+                            <p className='mt-2 text-center'>Phones and tablets</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
