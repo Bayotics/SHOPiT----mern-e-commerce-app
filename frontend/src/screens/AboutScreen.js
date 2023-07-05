2a7bc292284377a49084ff5f2403ec719bbf3518
bayoticstudios <abdullahishobaloju@gmail.com>
2023-07-05 09:46:59 +0100

    about screen ui

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -2,7 +2,12 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import { Helmet } from 'react-helmet-async';
-
+import about1 from '../assets/about1.png'
+import about2 from '../assets/about2.png'
+import about3 from '../assets/about3.png'
+import cart5 from '../assets/hero-7.jpg';
+import cart2 from '../assets/hero-8.jpg';
+import cart4 from '../assets/cart4.jpg'
 
 const AboutScreen = () => {
     return(
@@ -25,69 +30,86 @@ const AboutScreen = () => {
                     discover anything they might want to buy online.</p>
                 </Col>
                 <Col>
-                    <h4>Our Vision</h4>
+                    <h4>Our Mission</h4>
                     <p className='text-muted'>Our aim at SHOPiT is to listen to our customers and deliver a top tier service 
                     that allows users to feel the pride, respect and trust of everyone at our studio. 
                     We focus on guiding employees to create high-standard services and deliver excellent results..</p>
                 </Col>
             </Row>
-            <h3 className='text-center'>Meet Our Team</h3>
-                <Row>
-                    <Col className='justify-content-center'>
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-                        <h2 className="fw-normal">Heading</h2>
-                        <p>Team Lead</p>
-                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                    </Col>
-                    <Col>
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-                        <h2 className="fw-normal">Heading</h2>
-                        <p>Product Manager</p>
-                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                    </Col>
-                    <Col>
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-                        <h2 className="fw-normal">Heading</h2>
-                        <p>Head Designer</p>
-                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
-                    </Col>
-                </Row>
                 <hr className="featurette-divider" />
+
                 <div className="row featurette ">
-                <div className="col-md-7">
-                    <h2 className="featurette-heading fw-normal lh-1">First featurette heading. <span className="text-body-secondary">It’ll blow your mind.</span></h2>
-                    <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
-                </div>
-                <div className="col-md-5">
-                    <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
-                </div>
-                </div>
-
-                <hr className="featurette-divider" />
-
-                <div className="row featurette">
-                <div className="col-md-7 order-md-2">
-                    <h2 className="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span className="text-body-secondary">See for yourself.</span></h2>
-                    <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-                </div>
-                <div className="col-md-5 order-md-1">
-                    <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
-                </div>
+                    <div className="col-md-7">
+                        <h2 className="featurette-heading fw-normal lh-1">Fast and Easy Payments.</h2>
+                        <p className="lead">Make Payments for your favorite products with ease. Any time, wherever you are in the world</p>
+                    </div>
+                    <div className="col-md-5">
+                        <div className='border'>
+                            <img src = {cart2} alt = "cart1" style={{height: '500px', width: '100%'}} />
+                        </div>
+                    </div>
                 </div>
 
                 <hr className="featurette-divider" />
 
                 <div className="row featurette">
-                <div className="col-md-7">
-                    <h2 className="featurette-heading fw-normal lh-1">And lastly, this one. <span className="text-body-secondary">Checkmate.</span></h2>
-                    <p className="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
-                </div>
-                <div className="col-md-5">
-                    <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
-                </div>
+                    <div className="col-md-7 order-md-2">
+                        <h2 className="featurette-heading fw-normal lh-1">Best Prices<span className="text-body-secondary"></span></h2>
+                        <p className="lead">Every Product in our store is at a discounted price. You won't find them that Cheap anywhere</p>
+                    </div>
+                    <div className="col-md-5 order-md-1">
+                        <div className='border'>
+                            <img src = {cart5} alt = "cart1" style={{height: '500px', width: '100%'}} />
+                        </div>
+                    </div>
                 </div>
 
                 <hr className="featurette-divider" />
+
+                <div className="row featurette">
+                    <div className="col-md-7">
+                        <h2 className="featurette-heading fw-normal lh-1">Swift Delivery</h2>
+                        <p className="lead">We deliver swiftly to over 100 countries around the world.
+                        We hope you’ll love every purchase, but if you ever need to return an item you can do so within a month of receipt</p>
+                    </div>
+                    <div className="col-md-5">
+                        <div className='border'>
+                            <img src = {cart4} alt = "cart1" style={{height: '500px', width: '100%'}} />
+                        </div>
+                    </div>
+                </div>
+                <hr className="featurette-divider" />
+
+                <h3 className='text-center'>Meet Our Team</h3>
+                <div className='d-flex justify-content-around team-main'>
+                    <div className='text-center'>
+                        <div className="team-circle">
+                            <img src={about3} 
+                             style={{height: '250px', width: '250px', borderRadius: '50%', marginTop: '15px'}}
+                             alt = "about-one" />
+                        </div>
+                        <h2 className="fw-normal">Unclebayotics</h2>
+                        <p>Team Lead</p>
+                    </div>
+                    <div className='text-center'>
+                        <div className="team-circle">
+                            <img src={about1}
+                             style={{height: '250px', width: '250px', borderRadius: '50%', marginTop: '15px'}} 
+                             alt = "about-one" />
+                        </div>
+                        <h2 className="fw-normal">Bayospending</h2>
+                        <p>Product manager</p>
+                    </div>
+                    <div className='text-center'>
+                        <div className="team-circle">
+                            <img src={about2} 
+                            style={{height: '250px', width: '250px', borderRadius: '50%', marginTop: '15px'}} 
+                            alt = "about-one" />
+                        </div>
+                        <h2 className="fw-normal">John Doe</h2>
+                        <p>Head designer</p>
+                    </div>
+                </div>
 </div>
 
     )
