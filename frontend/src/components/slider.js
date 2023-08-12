643ca52d3fea88f4f15cfc459f281562bd0325e2
bayoticstudios <abdullahishobaloju@gmail.com>
2023-08-12 23:49:58 +0100

    d

--- frontend/src/components/slider.js
+++ frontend/src/components/slider.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 function Slider() {
   return (
     <Carousel>
-      <Carousel.Item interval={5000}>
+      <Carousel.Item interval={1000}>
         <img
           className="d-block w-100"
           src={bgOne}
