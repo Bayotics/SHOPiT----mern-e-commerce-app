8c1e150d53c64370fb991bfab52bfe2f554f017c
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2023-10-27 01:53:24 +0100

    landing category complete

--- frontend/src/components/LandingPageCategory.js
+++ frontend/src/components/LandingPageCategory.js
@@ -2,17 +2,33 @@
 
 const LandingPageCategory = () => {
     return(
-        <div className="landing-category-main border">
-            <div className="category-heading border"><h1 className="">Top <span className="tect-black">Categories</span></h1></div>
-            <div className="row border category-tiles">
-                <div className="col border category-tiles-first">
+        <div className="landing-category-main">
+            <div className="category-heading"><h1 className="">Top <span className="tect-black">Categories</span></h1></div>
+            <div className="row category-tiles">
+                <div className="col category-tiles-first">
                     <div className="category-tiles-inner">
                         <h1>All fish</h1>
-                        <button className="category-tiles-btn">Shop All</button>
+                        <div className="category-tiles-btn-main">
+                            <button className="category-tiles-btn">Shop All</button>
+                        </div>
                     </div>
                 </div>
-                <div className="col border category-tiles-mid">b</div>
-                <div className="col border category-tiles-last">c</div>
+                <div className="col category-tiles-mid">
+                    <div className="category-tiles-inner">
+                        <h1>Shellfish</h1>
+                        <div className="category-tiles-btn-main">
+                            <button className="category-tiles-btn">Shop Shellfish</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col category-tiles-last">
+                    <div className="category-tiles-inner">
+                        <h1>Smoked/Dried</h1>
+                        <div className="category-tiles-btn-main">
+                            <button className="category-tiles-btn">Shop smoked/dried</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
