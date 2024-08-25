b4c69d8eb9977f37159b8a3d2616e79d3ebd5c6b
bayoticstudios <abdullahishobaloju@gmail.com>
2024-08-25 22:29:40 +0100

    jwt update

--- backend/utils.js
+++ backend/utils.js
@@ -20,9 +20,11 @@ export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    console.log(token)
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         res.status(401).send({ message: 'Invalid Token' });
+        console.log(token)
       } else {
         req.user = decode;
         next();
